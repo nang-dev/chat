c09b220f6cb50a7bd6da7065e7aebfc30ef5418c
Nathan Ang <nathanang2000@gmail.com>
2015-06-10 13:21:48 -0700

    Update angular-chat.js

--- angular-chat.js
+++ angular-chat.js
@@ -120,7 +120,26 @@ function chatCtrl($rootScope, $http) {
     }
 
 
-
+        /*
+    Handle the emoji replacements
+    */
+    $rootScope.message.text = $rootScope.message.text.replaceAll(">:(", "😡");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(">:)", "😈");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":)", "😊");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":D", "😃");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":o", "😱");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":O", "😱");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":p", "😛");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":P", "😛");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":')", "😅");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":'(", "😢");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":'D", "😂");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":|", "😁");
+    $rootScope.message.text = $rootScope.message.text.replaceAll(":*", "😘");
+    $rootScope.message.text = $rootScope.message.text.replaceAll("<3", "💜");
+    $rootScope.message.text = $rootScope.message.text.replaceAll("</3", "💔");
+    
+    $rootScope.message.text = $rootScope.message.text + "/n" + "----------------------------------------";
 
 
     //set the message date
@@ -186,5 +205,11 @@ function chatCtrl($rootScope, $http) {
     $("#inputMessage").removeClass("error");
     $rootScope.clearMsg();
     $rootScope.loggedIn = false;
+  }
+  String.prototype.replaceAll = function(search, replace) {
+      if (replace === undefined) {
+          return this.toString();
+      }
+      return this.split(search).join(replace);
   }
 }
